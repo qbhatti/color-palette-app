c5d9263029923a481b64d294a7a97f519138a730
qbhatti <qasimbhatti@hotmail.com>
2020-01-05 13:41:36 -0800

    Styled Slider

--- src/Palette.js
+++ src/Palette.js
@@ -28,7 +28,9 @@ class Palette extends Component {
 
         return (
             <div className="Palette">
-                <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+                <div className="Palette-slider">
+                    <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel} />
+                </div>
                 {/* Navbar goes here */}
                 <div className="Palette-colors">
                     {colorBoxes}
